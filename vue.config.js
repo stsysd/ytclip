a1a18765b33adb127e7b677c3ff856ed05aee034
stsysd <000stsysd000@gmail.com>
2020-11-29 05:41:22 +0900

    update title

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: '@import "./src/assets/bulma.scss";'
-      }
-    }
-  },
   pages: {
     index: {
       entry: "src/main.ts",
-      title: "INDEX"
+      title: "ytclip"
     }
   }
 };
